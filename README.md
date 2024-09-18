eec3b5c4695d97899462805d955e9ca1ccbe6adf
Tilmann <zoodb@gmx.de>
2024-09-18 15:30:44 +0200

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # scion-java-packet-example
+
+This is a simple example project that uses [JPAN](https://github.com/scionproto-contrib/jpan) to send a packet to the [SCION packet analyzer](https://echoscion.ddns.net/).
+
+
+
+
+# Troubleshooting
+
+## No DNS search domain found. Please check your /etc/resolv.conf or similar.
+This happens, for example, on Windows when using a VPN. One solution is to execute the jar with the following property (
+the example works only for `ethz.ch`):
+
+```
+java -Dorg.scion.dnsSearchDomains=ethz.ch. -jar target/scion-multiping-0.0.1-ALPHA-SNAPSHOT-shaded.jar
+```
---
+++ src/main/java/org/scion/demo/ScionPacketExample.java
@@ -0,0 +1,39 @@
+// Copyright 2024 ETH Zurich
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.scion.demo;
+
+import org.scion.jpan.*;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
+
+public class ScionPacketExample {
+    public static void main(String[] args) throws IOException {
+        System.setProperty(Constants.PROPERTY_DNS_SEARCH_DOMAINS, "ethz.ch.");
+        long dstIsdAs = ScionUtil.parseIA("64-2:0:9");
+        InetAddress ip = InetAddress.getByName("129.132.175.104");
+        Path path = Scion.defaultService().getPaths(dstIsdAs, ip, Constants.SCMP_PORT).get(0);
+
+        try (ScionDatagramChannel channel = ScionDatagramChannel.open()) {
+            channel.configureBlocking(true);
+            channel.connect(path);
+            ByteBuffer sendBuf = ByteBuffer.wrap("Hello there! Hello!".getBytes());
+            channel.write(sendBuf);
+            PathMetadata meta = channel.getConnectionPath().getMetadata();
+            System.out.println("Sent via path: " + ScionUtil.toStringPath(meta));
+        }
+    }
+}
